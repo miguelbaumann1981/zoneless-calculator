0b70fb617266997778156b229c77446304b331fd
MiguelDev <miguelmartinezm@outlook.es>
2024-11-29 16:19:04 +0100

    calculator works

---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,55 @@
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [
+    CalculatorButtonComponent
+  ],
+  templateUrl: './calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)' 
+  }
+})
+export class CalculatorComponent {
+
+  private calculatorService = inject(CalculatorService);
+
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+
+  // get resultText() {
+  //   return this.calculatorService.resultText();
+  // }
+
+
+  handleClick(key: string) {
+    this.calculatorService.constructNumber(key);
+  }
+
+  handleKeyboardEvent(event: KeyboardEvent) {
+
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      X: '*',
+      '/': '÷',
+      Enter: '='
+    };
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach(button => {
+      button.keyboardPressedStyle(keyValue);
+    })
+    
+  }
+
+ }
